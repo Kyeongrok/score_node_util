e7ed63641f26b26d2df935bf626a5e598adc09a5
krkim <kyeongrok.kim@okiconcession.com>
2017-02-11 15:02:03 +0900

    시간 추가

--- main_crawler.js
+++ main_crawler.js
@@ -29,37 +29,46 @@ var signature = SHA256(apiKey + secret +  Math.floor((new Date().getTime()) / 10
 var beforeRes = "";
 var beforeMessage = "";
 
+let serviceFn = (res)=>{
+
+  if(beforeRes != res){
+    let fileName = "./saved/" + dateTime.create().format('Y-m-d-H-M-S') + ".json";
+    let json = JSON.parse(res);
+    let teams = json['apiResults'][0]['league']['season']['eventType'][0]['events'][0]['teams'];
+
+    let team_1 = teams[0];
+    let team_2 = teams[1];
+    console.log(teams);
+    let message = team_1['displayName'] + " linescores:" + team_1['linescores'] + team_2['displayName'] + " linescores:" + team_2['linescores'];
+    crawler.writeFile(res, fileName);
+    console.log(beforeMessage);
+    console.log(message);
+    console.log(beforeMessage != message);
+    if( beforeMessage != message){
+      bot.sendMessage("-202009157", JSON.stringify(message));
+      beforeMessage = message;
+    }
+  }
+}
+
 //2.parse를 호출한다. callback으로 fileWriter를 넘긴다.
 let getSetInterval = ()=>{
     return setInterval(()=>{
       //1.주소를 만든다
+
       let url = util.format(footballLiveFormat, "1643374", apiKey, signature)
       console.log(url);
-      crawler.parse(url, (res)=>{
 
-        if(beforeRes != res){
-          let fileName = "C:/Users/kyeongrok.kim/Desktop/saved/" + dateTime.create().format('Y-m-d-H-M-S') + ".json";
-          let json = JSON.parse(res);
-          let teams = json['apiResults'][0]['league']['season']['eventType'][0]['events'][0]['teams'];
+      let nowDate = new Date();
 
-          let team_1 = teams[0];
-          let team_2 = teams[1];
-          console.log(teams);
-          let message = team_1['displayName'] + " linescores:" + team_1['linescores'] + team_2['displayName'] + " linescores:" + team_2['linescores'];
-          //crawler.writeFile(res, fileName);
-          console.log(beforeMessage);
-          console.log(message);
-          console.log(beforeMessage != message);
-          if( beforeMessage != message){
-            bot.sendMessage("-202009157", JSON.stringify(message));
-            beforeMessage = message;
-          }
-        }
+      let startDate = new Date("2017", "1", "11", "21","30","0");
+      let endDate = new Date("2017", "1", "11", "24","00","0");
 
+      if(startDate < nowDate && nowDate < endDate ){
+          crawler.parse(url, serviceFn );
+      }
 
-
-      });
-    }, 1000); //여기 1000이 1초이다. 10초로 하고 싶으면 10000으로 하면 된다.
+    }, 3000); //여기 1000이 1초이다. 10초로 하고 싶으면 10000으로 하면 된다.
 }
 
 let hello = getSetInterval();
