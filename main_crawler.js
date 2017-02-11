60c88e48e89db537a6dc91e9d4dc7f8914f21b82
krkim <kyeongrok.kim@okiconcession.com>
2017-02-11 23:24:34 +0900

    여러가지 조건 및 필터 추가

--- main_crawler.js
+++ main_crawler.js
@@ -25,29 +25,58 @@ var bot = new TelegramBot(token, { polling: false });
 
 //sha256으로 encoding한다.
 var signature = SHA256(apiKey + secret +  Math.floor((new Date().getTime()) / 1000) )
-
 var beforeRes = "";
 var beforeMessage = "";
+var beforePbpLength = 0;
 
 let serviceFn = (res)=>{
 
-  if(beforeRes != res){
-    let fileName = "./saved/" + dateTime.create().format('Y-m-d-H-M-S') + ".json";
-    let json = JSON.parse(res);
-    let teams = json['apiResults'][0]['league']['season']['eventType'][0]['events'][0]['teams'];
+  let fileName = "./saved/" + dateTime.create().format('Y-m-d-H-M-S') + ".json";
+  let json = JSON.parse(res);
+  let selectedEvent = json['apiResults'][0]['league']['season']['eventType'][0]['events'][0];
+  let teams = selectedEvent['teams'];
+  let pbp = selectedEvent['pbp'];
+  let eventStatus = selectedEvent['eventStatus'];
+  let minutes = eventStatus['time']['minutes'];
+  let seconds = eventStatus['time']['seconds'];
 
-    let team_1 = teams[0];
-    let team_2 = teams[1];
-    console.log(teams);
-    let message = team_1['displayName'] + " linescores:" + team_1['linescores'] + team_2['displayName'] + " linescores:" + team_2['linescores'];
+  let boxscores = selectedEvent['boxscores'];
+  let boxscores_1 = boxscores[0];
+  let boxscores_2 = boxscores[1];
+
+  let teamStats_1 = boxscores_1['teamStats'];
+  let teamStats_2 = boxscores_2['teamStats'];
+
+  let yellowCards_1 = teamStats_1['yellowCards']
+  let yellowCards_2 = teamStats_2['yellowCards']
+  let redCards_1 = teamStats_1['redCards']
+  let redCards_2 = teamStats_2['redCards']
+
+  let pbpLength = pbp.length;
+  let team_1 = teams[0];
+  let team_2 = teams[1];
+  let linescore_1 = team_1['linescores'];
+  let linescore_2 = team_2['linescores'];
+
+  let message = JSON.stringify(eventStatus) + " "
+  + team_1['displayName']
+  + " linescores:" + JSON.stringify(linescore_1)
+  + team_2['displayName']
+  + " linescores:" + JSON.stringify(linescore_2)
+  + " pbp:"+ JSON.stringify(pbp[pbpLength-1]['playText'])
+  + " yello_1:" + yellowCards_1 + " yello_2:" + yellowCards_2
+  + " red_1:" + redCards_1 + " red_2:" + redCards_2
+  //+ " teamStats_1:"+ JSON.stringify(teamStats_1)
+  //+ " teamStats_2:"+ JSON.stringify(teamStats_2)
+  ;
+
+  console.log(minutes + ":" + seconds);
+  if(beforeMessage != message){
     crawler.writeFile(res, fileName);
-    console.log(beforeMessage);
     console.log(message);
-    console.log(beforeMessage != message);
-    if( beforeMessage != message){
-      bot.sendMessage("-202009157", JSON.stringify(message));
-      beforeMessage = message;
-    }
+    bot.sendMessage("-202009157", message);
+    beforeMessage = message;
+    beforePbpLength = pbpLength;
   }
 }
 
@@ -55,12 +84,8 @@ let serviceFn = (res)=>{
 let getSetInterval = ()=>{
     return setInterval(()=>{
       //1.주소를 만든다
-
       let url = util.format(footballLiveFormat, "1643374", apiKey, signature)
-      console.log(url);
-
       let nowDate = new Date();
-
       let startDate = new Date("2017", "1", "11", "21","30","0");
       let endDate = new Date("2017", "1", "11", "24","00","0");
 
@@ -68,7 +93,7 @@ let getSetInterval = ()=>{
           crawler.parse(url, serviceFn );
       }
 
-    }, 3000); //여기 1000이 1초이다. 10초로 하고 싶으면 10000으로 하면 된다.
+    }, 4000); //여기 1000이 1초이다. 10초로 하고 싶으면 10000으로 하면 된다.
 }
 
 let hello = getSetInterval();
