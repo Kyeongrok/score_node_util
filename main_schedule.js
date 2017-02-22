9b9b4261754e4d9e93bb9dd89122eff98d6d3ffa
OKICONCESSION\kyeongrok.kim <kyeongrok.kim@okiconcession.com>
2017-02-22 13:17:22 +0900

    utc -> gmt로 변경 하는 기능 등 추가

--- main_schedule.js
+++ main_schedule.js
@@ -1,10 +1,12 @@
 /*
+/*
 1.url 추출
 2.url로 파일을 만듬
 3.excel로 저장
 */
 
 var FileHandler = require('./FileHandler.js');
+const excelExporter = require("./ExcelExporter.js")
 
 const statsUrlFormats = require("./statsUrlFormats.js");
 
@@ -22,16 +24,8 @@ console.log(statsBaseballNpbScheduleUrl)
 var testDataLocation = "./data/";
 var fh = new FileHandler(testDataLocation);
 var fileList = fh.fileList;
-//console.log(fileList);
+console.log(fileList);
 
-var string = fh.getFileContentsString(testDataLocation + "stats_mlb_schedule_170221_170331.json");
-var json = fh.getJsonObject(string);
-
-
-let events = json['apiResults'][0]['league']['season']['eventType'][0]['events'];
-
-for (var event of events){
-  //console.log("-----");
-  //console.log(event);
-
-}
+var npbSchedule = fh.getFileContentsString(testDataLocation + "stats_npb_schedule_17021_170331.json");
+var mlbSchedule = fh.getFileContentsString(testDataLocation + "stats_mlb_schedule_17021_170331.json");
+excelExporter.scheduleExport(npb, "./npb.xlsx", "test");
