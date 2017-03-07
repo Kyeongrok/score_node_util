3cda9017492744359425e0cb6321739da5aeb03e
krkim <kyeongrok.kim@okiconcession.com>
2017-03-08 01:01:56 +0900

    8경기 크롤링 등록

--- main_crawler.js
+++ main_crawler.js
@@ -12,17 +12,40 @@ var beforeRes = "";
 var beforeMessage = "";
 var beforePbpLength = 0;
 
-let serviceFn = (res)=>{
-  let fileName = "./saved/baseball/espn/" + dateTime.create().format('Y-m-d-H-M-S') + ".html";
+
+let serviceFn1 = (res)=>{
+  let fileName = "./saved/baseball/mlb/1/" + dateTime.create().format('Y-m-d-H-M-S') + ".json";
   crawler.writeFile(res, fileName);
 }
-
 let serviceFn2 = (res)=>{
-  let fileName = "./saved/baseball/mlb/" + dateTime.create().format('Y-m-d-H-M-S') + ".json";
+  let fileName = "./saved/baseball/mlb/2/" + dateTime.create().format('Y-m-d-H-M-S') + ".json";
+  crawler.writeFile(res, fileName);
+}
+let serviceFn3 = (res)=>{
+  let fileName = "./saved/baseball/mlb/3/" + dateTime.create().format('Y-m-d-H-M-S') + ".json";
+  crawler.writeFile(res, fileName);
+}
+let serviceFn4 = (res)=>{
+  let fileName = "./saved/baseball/mlb/4/" + dateTime.create().format('Y-m-d-H-M-S') + ".json";
+  crawler.writeFile(res, fileName);
+}
+let serviceFn5 = (res)=>{
+  let fileName = "./saved/baseball/mlb/5/" + dateTime.create().format('Y-m-d-H-M-S') + ".json";
+  crawler.writeFile(res, fileName);
+}
+let serviceFn6 = (res)=>{
+  let fileName = "./saved/baseball/mlb/6/" + dateTime.create().format('Y-m-d-H-M-S') + ".json";
+  crawler.writeFile(res, fileName);
+}
+let serviceFn7 = (res)=>{
+  let fileName = "./saved/baseball/mlb/7/" + dateTime.create().format('Y-m-d-H-M-S') + ".json";
+  crawler.writeFile(res, fileName);
+}
+let serviceFn8 = (res)=>{
+  let fileName = "./saved/baseball/mlb/8/" + dateTime.create().format('Y-m-d-H-M-S') + ".json";
   crawler.writeFile(res, fileName);
 }
 
-let getUrl = (eventId)=>("http://www.espn.com/mlb/playbyplay?gameId=" + eventId)
 let getUrl2 = (eventId)=>("http://statsapi.mlb.com/api/v1/game/" +eventId+ "/feed/live?language=en" )
 
 //2.parse를 호출한다. callback으로 fileWriter를 넘긴다.
@@ -47,11 +70,16 @@ let getSetInterval = (schedule, serviceFn, urlGetter)=>{
 }
 
 
-console.log(new Date())
 
-mlb_espn = getSetInterval(scheduleList['scheduleList'][3], serviceFn, getUrl);
-mlb_mlb = getSetInterval(scheduleList['scheduleList'][4], serviceFn2, getUrl2);
-//509566
+mlb_mlb_1 = getSetInterval(scheduleList['scheduleList'][0], serviceFn1, getUrl2);
+mlb_mlb_2 = getSetInterval(scheduleList['scheduleList'][1], serviceFn2, getUrl2);
+mlb_mlb_3 = getSetInterval(scheduleList['scheduleList'][2], serviceFn3, getUrl2);
+mlb_mlb_4 = getSetInterval(scheduleList['scheduleList'][3], serviceFn4, getUrl2);
+mlb_mlb_5 = getSetInterval(scheduleList['scheduleList'][4], serviceFn5, getUrl2);
+mlb_mlb_6 = getSetInterval(scheduleList['scheduleList'][5], serviceFn6, getUrl2);
+mlb_mlb_7 = getSetInterval(scheduleList['scheduleList'][6], serviceFn7, getUrl2);
+mlb_mlb_8 = getSetInterval(scheduleList['scheduleList'][7], serviceFn8, getUrl2);
+
 
 ///forever start -o /var/www/score_node_util/out.log main_crawler.js
 //node /var/www/score_node_util/out.log -o /var/www/score_node_util/out.log main_crawler.js
--- scheduleList.js
+++ scheduleList.js
@@ -2,39 +2,62 @@
 //month는 0부터이므로 -1해줄 것
 
 const scheduleList = [
-
-    {"eventId":"1705947", "sYear":"2017", "sMonth":"2", "sDate":"24"
-        , "sHour":"18"  ,"sMinute":"5"  ,"sSecond":"0"
-        , "eYear":"2017", "eMonth":"2", "eDate":"24"
-        , "eHour":"22"  ,"eMinute":"0"  ,"eSecond":"0"
+    {"eventId":"509877", "sYear":"2017", "sMonth":"3", "sDate":"7"
+        , "sHour":"18"  ,"sMinute":"20"  ,"sSecond":"0"
+        , "eYear":"2017", "eMonth":"3", "eDate":"7"
+        , "eHour":"21"  ,"eMinute":"50"  ,"eSecond":"0"
         , "league":"mlb", "home_team_name":"boston", "away_team_name":"newyork"
+        , "site":"mlb"
     },
-    {"eventId":"1724739", "sYear":"2017", "sMonth":"3", "sDate":"1"
-        , "sHour":"9"  ,"sMinute":"0"  ,"sSecond":"0"
-        , "eYear":"2017", "eMonth":"3", "eDate":"1"
-        , "eHour":"12"  ,"eMinute":"0"  ,"eSecond":"0"
-        , "league":"npb", "home_team_name":"boston", "away_team_name":"newyork"
+    {"eventId":"510015", "sYear":"2017", "sMonth":"3", "sDate":"7"
+        , "sHour":"18"  ,"sMinute":"20"  ,"sSecond":"0"
+        , "eYear":"2017", "eMonth":"3", "eDate":"7"
+        , "eHour":"21"  ,"eMinute":"50"  ,"eSecond":"0"
+        , "league":"mlb", "home_team_name":"boston", "away_team_name":"newyork"
+        , "site":"mlb"
     },
-    {"eventId":"1724740", "sYear":"2017", "sMonth":"3", "sDate":"2"
-        , "sHour":"9"  ,"sMinute":"0"  ,"sSecond":"0"
-        , "eYear":"2017", "eMonth":"3", "eDate":"0"
-        , "eHour":"12"  ,"eMinute":"0"  ,"eSecond":"0"
-        , "league":"npb", "home_team_name":"boston", "away_team_name":"newyork"
+    {"eventId":"509948", "sYear":"2017", "sMonth":"3", "sDate":"7"
+        , "sHour":"18"  ,"sMinute":"20"  ,"sSecond":"0"
+        , "eYear":"2017", "eMonth":"3", "eDate":"7"
+        , "eHour":"21"  ,"eMinute":"50"  ,"eSecond":"0"
+        , "league":"mlb", "home_team_name":"boston", "away_team_name":"newyork"
+        , "site":"mlb"
     },
-    {"eventId":"370307106", "sYear":"2017", "sMonth":"3", "sDate":"6"
-        , "sHour":"18"  ,"sMinute":"0"  ,"sSecond":"0"
-        , "eYear":"2017", "eMonth":"3", "eDate":"6"
-        , "eHour":"21"  ,"eMinute":"0"  ,"eSecond":"0"
-        , "league":"npb", "home_team_name":"boston", "away_team_name":"newyork"
+    {"eventId":"509671", "sYear":"2017", "sMonth":"3", "sDate":"7"
+        , "sHour":"18"  ,"sMinute":"20"  ,"sSecond":"0"
+        , "eYear":"2017", "eMonth":"3", "eDate":"7"
+        , "eHour":"21"  ,"eMinute":"50"  ,"eSecond":"0"
+        , "league":"mlb", "home_team_name":"boston", "away_team_name":"newyork"
+        , "site":"mlb"
     },
-    {"eventId":"509670", "sYear":"2017", "sMonth":"3", "sDate":"6"
-        , "sHour":"18"  ,"sMinute":"0"  ,"sSecond":"0"
-        , "eYear":"2017", "eMonth":"3", "eDate":"6"
-        , "eHour":"21"  ,"eMinute":"0"  ,"eSecond":"0"
-        , "league":"npb", "home_team_name":"boston", "away_team_name":"newyork"
+    {"eventId":"509982", "sYear":"2017", "sMonth":"3", "sDate":"7"
+        , "sHour":"20"  ,"sMinute":"20"  ,"sSecond":"0"
+        , "eYear":"2017", "eMonth":"3", "eDate":"7"
+        , "eHour":"23"  ,"eMinute":"50"  ,"eSecond":"0"
+        , "league":"mlb", "home_team_name":"boston", "away_team_name":"newyork"
+        , "site":"mlb"
     },
-
-
+    {"eventId":"509617", "sYear":"2017", "sMonth":"3", "sDate":"7"
+        , "sHour":"20"  ,"sMinute":"20"  ,"sSecond":"0"
+        , "eYear":"2017", "eMonth":"3", "eDate":"7"
+        , "eHour":"23"  ,"eMinute":"50"  ,"eSecond":"0"
+        , "league":"mlb", "home_team_name":"boston", "away_team_name":"newyork"
+        , "site":"mlb"
+    },
+    {"eventId":"509734", "sYear":"2017", "sMonth":"3", "sDate":"7"
+        , "sHour":"20"  ,"sMinute":"20"  ,"sSecond":"0"
+        , "eYear":"2017", "eMonth":"3", "eDate":"7"
+        , "eHour":"23"  ,"eMinute":"50"  ,"eSecond":"0"
+        , "league":"mlb", "home_team_name":"boston", "away_team_name":"newyork"
+        , "site":"mlb"
+    },
+    {"eventId":"509512", "sYear":"2017", "sMonth":"3", "sDate":"7"
+        , "sHour":"20"  ,"sMinute":"30"  ,"sSecond":"0"
+        , "eYear":"2017", "eMonth":"3", "eDate":"7"
+        , "eHour":"23"  ,"eMinute":"59"  ,"eSecond":"0"
+        , "league":"mlb", "home_team_name":"boston", "away_team_name":"newyork"
+        , "site":"mlb"
+    },
 
 ]
 exports.scheduleList = scheduleList;
