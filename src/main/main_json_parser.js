c195be239ece55064b6ce3a085914e3571329e3b
kyeongrok.kim <kyeongrokkim@okiconcession.com>
2017-10-16 19:57:49 +0900

    print score

--- src/main/main_json_parser.js
+++ src/main/main_json_parser.js
@@ -1,18 +1,31 @@
+const client = require('cheerio-httpcli');
+
 const crawlerMaker = require("./crawler/crawlerMaker.js");
 const statsBasketballParser = (string) => {
   const json = JSON.parse(string)
   const event = json.apiResults[0].league.season.eventType[0].events[0];
   const teams = event.teams;
-  console.log('status %s ha score: %s vs %s', event.eventStatus.eventStatusId, teams[0].score, teams[1].score);
+  const pbp = event.pbp;
+  const nowPbp = pbp[pbp.length - 1];
+
+  console.log('status %s period:%s ha score: %s vs %s homeFouls:%s visitorFouls:%s',
+    event.eventStatus.eventStatusId, event.eventStatus.period,
+    teams[0].score, teams[1].score, nowPbp.homeFouls, nowPbp.visitorFouls);
+
 }
 
-const crawler = crawlerMaker.getCrawler(statsBasketballParser);
+const crawl = crawlerMaker.getCrawler(statsBasketballParser);
 
-for(let i = 1076; i < 1077 ; i++){
+for(let i = 1113; i < 3000 ; i += 1){
   const url = 'http://13.124.67.68:8203/basketball/stats/game1?eventId=' + i;
   console.log(url);
-  crawler(url);
 
+  try {
+    // crawl(url);
+    const response = client.fetchSync(url);
+    statsBasketballParser(response.body);
+  } catch (e) {
+  }
 }
 
 
