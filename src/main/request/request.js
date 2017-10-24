5d3483507324953903ba6db077733bfe61285115
kyeongrok.kim <kyeongrokkim@okiconcession.com>
2017-10-24 14:45:35 +0900

    CallAndParse 추가

--- src/main/request/request.js
+++ src/main/request/request.js
@@ -1,5 +1,4 @@
 const request = require('request');
-const iconv = require('iconv-lite');
 
 exports.getString = callback => url => request({
   url,
@@ -12,12 +11,7 @@ exports.getString = callback => url => request({
 },
 (error, response, body) => {
   if (!error && response.statusCode === 200) {
-    iconv.extendNodeEncodings();
-    const strContents = new Buffer(body);
-    console.log(strContents);
-    console.log(iconv.decode(strContents, 'EUC-KR').toString());
-
-
+    console.log(url);
     callback(body);
   } else {
     console.log(`error${response.statusCode}`);
--- src/main/services/naverCallAndSave.js
+++ src/main/services/naverCallAndSave.js
@@ -1,10 +1,10 @@
 const callAndParse = require('../saver/callAndParse');
+const fs = require('fs');
 
 let url = 'http://sportsdata.naver.com/data//kbl_game/2017/10/20171015553031016.nsd';
 url = 'http://sportsdata.naver.com/data//kbl_game/2017/10/201710241050310121.nsd';
-
-
-setInterval(() => {
-  callAndParse.callAndParse(url)(string => console.log(string));
-}, 1000 * 1);
+callAndParse.callAndParse(url)((string) => {
+  console.log(string);
+  fs.writeFile('./naver_basketball_201710241050310121.json', string);
+});
 
